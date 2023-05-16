3241f40bece6a381dd9febb60600a5c601af759b
Padraig Donnelly <padraig2.pd@gmail.com>
2023-05-16 12:16:54 +0200

    Update run_obr_v4.py

--- run_obr_v4.py
+++ run_obr_v4.py
@@ -1,15 +1,14 @@
 import subprocess
-from sorcery import dict_of
 
-def build_command(path_in: str, year: int, month: int, day: int, iasi_channels: list, file_in: str, file_out: str):
-    run_dir = f"/home/pdonnelly/data/obr_v4"
-    filepath = f"-d {path_in}"
-    first_date = f"-fd {year:04d}-{month:02d}-{day:02d}"
-    last_date = f"-ld {year:04d}-{month:02d}-{day:02d}"
-    channels = f"c {iasi_channels[0]}-{iasi_channels[-1]}"
-    mf = f"-mf {file_in}"
-    suffix = f"of bin -out {file_out}"
-    return f"{run_dir} {filepath} {first_date} {last_date} {channels} {mf} {suffix}"
+def build_command(path_in: str, year: int, month: int, day: int, iasi_channels: list, filter: str, file_out: str):
+    run_dir = f"/home/pdonnelly/data/obr_v4 "
+    filepath = f"-d {path_in} "
+    first_date = f"-fd {year:04d}-{month:02d}-{day:02d} "
+    last_date = f"-ld {year:04d}-{month:02d}-{day:02d} "
+    channels = f"-c {iasi_channels[0]}-{iasi_channels[-1]} "
+    filter = "" #f"-mf {file_in}"
+    output = f"of bin -out {file_out} "
+    return f"{run_dir}{filepath}{first_date}{last_date}{channels}{filter}{output}"
 
 # Define inputs for OBR tool
 path_in = '/bdd/IASI/L1C/'
@@ -17,7 +16,7 @@ years = [2020]
 months = [1] #, 2, 3]
 days = [1] #[day for day in range(28)] 
 iasi_channels = [1, 2, 3]
-file_in = 'W_XX-EUMETSAT-Darmstadt,SOUNDING+SATELLITE,METOPA+IASI_C_EUMC_20200101000253_68496_eps_o_l1.bin'
+filter = 'W_XX-EUMETSAT-Darmstadt,SOUNDING+SATELLITE,METOPA+IASI_C_EUMC_20200101000253_68496_eps_o_l1.bin'
 file_out = 'file_out.bin'
 
 # Loop through date-times
@@ -26,9 +25,8 @@ for year in years:
         for day in days:
 
             # Construct command-line executable
-            command = build_command(path_in, year, month, day, iasi_channels, file_in, file_out)
-            
+            command = build_command(path_in, year, month, day, iasi_channels, filter, file_out)
             print(command)
-            
+
             # Execute OBR command
             subprocess.run(command, shell=True)
