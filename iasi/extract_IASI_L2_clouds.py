d1178396daa57d9a5a87e3a0db669923e4112026
Padraig Donnelly <padraig2.pd@gmail.com>
2023-06-06 12:48:52 +0200

    Update extract_IASI_L2_clouds.py

--- iasi/extract_IASI_L2_clouds.py
+++ iasi/extract_IASI_L2_clouds.py
@@ -52,14 +52,14 @@ class IASIExtractor:
             return f"{executable} {filepath} {first_date} {last_date} {channels} {filter} {output}"
         elif self.data_level == 'L2':
             executable = "/data/pdonnelly/IASI/scripts/BUFR_iasi_clp_reader_from20190514"
-            return f"{executable} {datafile_in} {self.datapath_out}{datafile_out}"
+            return f"{executable} {self.datapath_out} {datafile_in} {self.datapath_out}{datafile_out}"
         else:
             raise ValueError("Invalid data path type. Accepts 'L1C' or 'L2'.")
 
     def _run_data_processing(self, datafile_in: str, datafile_out: str):
         command = self._get_command(datafile_in, datafile_out)
         subprocess.run(['bash', command], check=True)
-        # print(command)
+        print(command)
 
     def _process_file(self, datafile_in: str):
         datafile_out = datafile_in.split(",")[2]
@@ -69,7 +69,7 @@ class IASIExtractor:
 
     def _get_datapath_in(self, month: str, day: str):
         if self.data_level == 'L1C':
-            return f"/bdd/metopc/l1c/iasi/{self.year}/{month}/{day}/"
+            return f"/bdd/metopc/l1c/iasi/{self.year}/{month}/{day}"
         elif self.data_level == 'L2':
             return f"/bdd/metopc/l2/iasi/{self.year}/{month}/{day}/clp"
         else:
@@ -81,19 +81,19 @@ class IASIExtractor:
             for datafile_in in os.scandir(datapath_in):
                 self._process_file(datafile_in.name)
 
-    def _rename_files_with_suffix(self, old_suffix: str, new_suffix: str):
-        if os.path.isdir(self.datapath_out):
-            for filename in os.scandir(self.datapath_out):
-                if filename.name.endswith(old_suffix):
-                    new_filename = f"{filename.name[:-len(old_suffix)]}{new_suffix}"
-                    os.rename(filename.path, os.path.join(self.datapath_out, new_filename))
-
     def process_files(self):
         for month in self.months:
             for day in range(1, self.days[month-1] + 1):
                 self.month = month
                 self.day = day
                 self._process_files_for_date(f"{month:02d}", f"{day:02d}")
+
+    def rename_files_with_suffix(self, old_suffix: str, new_suffix: str):
+        if os.path.isdir(self.datapath_out):
+            for filename in os.scandir(self.datapath_out):
+                if filename.name.endswith(old_suffix):
+                    new_filename = f"{filename.name[:-len(old_suffix)]}{new_suffix}"
+                    os.rename(filename.path, os.path.join(self.datapath_out, new_filename))
 
 def main():
     year = 2020
@@ -102,7 +102,7 @@ def main():
     
     data = IASIExtractor(year, months, days, data_level="L2")
     data.process_files()
-    data._rename_files_with_suffix(old_suffix=".bin", new_suffix=".out")
+    data.rename_files_with_suffix(old_suffix=".bin", new_suffix=".out")
 
 
 if __name__ == "__main__":
