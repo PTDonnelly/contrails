a14f24663060a44f66d39ca1dd43bd3b6d242338
Padraig Donnelly <padraig2.pd@gmail.com>
2023-06-19 17:52:24 +0200

    Update correlator.py

--- pisco_sandbox/pisco/correlator.py
+++ pisco_sandbox/pisco/correlator.py
@@ -92,9 +92,10 @@ class L1C_L2_Correlator:
         # Check that latitude, longitude, datetime, and local time are present in both file headers 
         self._check_headers()
 
-
+        print(self.df_l1c.columns)
         print(self.df_l1c['Latitude'])
         print("")
+        print(self.df_l2.columns)
         print(self.df_l2['Latitude'])
         print("")
         # Latitude and longitude values are rounded to 2 decimal places.
@@ -105,7 +106,9 @@ class L1C_L2_Correlator:
         # Merge two DataFrames based on latitude, longitude and datetime,
         # rows from df_l1c that do not have a corresponding row in df_l2 are dropped.
         self.merged_df = pd.merge(self.df_l1c, self.df_l2, on=['Latitude', 'Longitude', 'Datetime'], how='inner')
-
+        print(self.merged_df.columns)
+        print(self.merged_df['Latitude'])
+        print("")
         # # Convert the DataFrame 'Local Time' column (np.array) to boolean values
         # merged_df['Local Time'] = merged_df['Local Time'].astype(bool)
         # # Split the DataFrame into two based on 'Local Time' column
