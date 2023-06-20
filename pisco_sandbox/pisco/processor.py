1a8d0aade3015009a3818cd041d56068119f023d
Padraig Donnelly <padraig2.pd@gmail.com>
2023-06-20 13:28:34 +0200

    Update processor.py

--- pisco_sandbox/pisco/processor.py
+++ pisco_sandbox/pisco/processor.py
@@ -185,8 +185,8 @@ class L1CProcessor:
             if (i < 8) or (field in self.targets):
                 print(f"Extracting: {field}")
 
-                header_start = self.header_size + 12 + cumsize
-                self.f.seek(header_start, 0)
+                field_start = self.header_size + 12 + cumsize
+                # self.f.seek(field_start, 0)
 
                 # Calculate the byte offset to the next measurement
                 byte_offset = self.record_size + 8 - dtype_size
@@ -197,7 +197,7 @@ class L1CProcessor:
                 # Read the data of each measurement
                 for measurement in range(self.number_of_measurements):
                     # Move the file pointer to the starting position of the current field
-                    # self.f.seek(header_start * self.skip_measurements * measurement, 0)
+                    self.f.seek(field_start * self.skip_measurements * measurement, 0)
                     
                     # Read bytes
                     value = np.fromfile(self.f, dtype=dtype, count=1, sep='', offset=byte_offset)
