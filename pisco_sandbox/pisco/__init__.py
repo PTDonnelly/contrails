83e07a707c940115a00802320b87dc06e2b9c161
Padraig Donnelly <padraig2.pd@gmail.com>
2023-06-21 15:34:13 +0200

    Update __init__.py

--- pisco_sandbox/pisco/__init__.py
+++ pisco_sandbox/pisco/__init__.py
@@ -2,4 +2,4 @@ from .configuration import Configurer
 from .extraction import Extractor
 from .preprocessing import Preprocessor
 from .processing import Processor
-from .scripts import process_iasi, correlate_l1c_l2
+from .scripts import process_iasi, process_iasi
