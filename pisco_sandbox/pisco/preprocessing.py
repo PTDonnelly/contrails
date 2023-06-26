0f7709471c9f1e9bc32025127de70dde37080d83
Padraig Donnelly <padraig2.pd@gmail.com>
2023-06-26 11:43:47 +0200

    Update preprocessing.py

--- pisco_sandbox/pisco/preprocessing.py
+++ pisco_sandbox/pisco/preprocessing.py
@@ -500,10 +500,10 @@ class Preprocessor:
             # Read L1C-specific record fields and add to DataFrame
             print("\nL1C Record Fields:")
             self.read_record_fields(self.metadata._get_iasi_l1c_record_fields())
-            print(self.data_record_df[['Start Channel 1', 'Start Channel 2', 'Start Channel 3']].head())
-            input()
-            print(self.data_record_df[['End Channel 1', 'End Channel 2', 'End Channel 3']].head())
-            input()
+            print(self.data_record_df.head())
+            # input()
+            # print(self.data_record_df[['End Channel 1', 'End Channel 2', 'End Channel 3']].head())
+            # input()
             exit()
             self.read_spectral_radiance(self.metadata._get_iasi_l1c_record_fields())
             
