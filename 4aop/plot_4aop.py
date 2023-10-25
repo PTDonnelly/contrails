eb4606bf341711158287875e9eb51d3ab76296ee
Padraig Donnelly <padraig2.pd@gmail.com>
2023-10-25 19:01:11 +0200

    Edit

--- 4aop/plot_4aop.py
+++ 4aop/plot_4aop.py
@@ -1,17 +1,22 @@
 import numpy as np
+import matplotlib.gridspec as gridspec
 import matplotlib.pyplot as plt
+import os
 
 # Constants
 DATA_DIR = "C:\\Users\\padra\\Documents\\Research\\projects\\contrails\\4aop\\outputs\\"
-INPUT_FILE = f"{DATA_DIR}spc4asub0001test_test_iasil1c_trophr20iasi1c.plt"
-OUTPUT_FILE = INPUT_FILE.split(".")[0]
-
-COLUMNS = [
-    (1, 'Radiance (W $m^{-2} sr^{-1} cm^{-1}$)', 'black'),
-    (2, 'Brightness Temperature (K)', 'red')
+INPUT_FILES = [
+    "spc4asub0001_inv_co2_CAMS_test_iasil1c_icehr20iasi1c.plt",
+    "spc4asub0001_inv_co2_CAMS_test_iasil1c_ice_test_icehr20iasi1c.plt"
 ]
-FIG_SIZE = (6, 3)
+OUTPUT_FILE = os.path.join(DATA_DIR, "ice_vs_noice.png")
+RADIANCE_COLUMN = 1
+LABELS = ['Cloud-free', 'Cirrus (Baum et al., 2014)']
+YLABELS = ['Radiance (W $m^{-2} sr^{-1} cm^{-1}$)', 'Residuals (%)']
+SCALE = 'linear'
+FIG_SIZE = (8, 6)
 FONT_SIZE = 10
+COLOURS = ['black', 'red']
 
 def read_plt_file(filename):
     """Load data from filename, handling possible errors."""
@@ -23,31 +28,55 @@ def read_plt_file(filename):
         print(f"An unexpected error occurred: {e}")
     return None
 
-def plot_column(ax, x_data, y_data, y_label, color):
+def plot_radiance(ax, x_data, y_data, label, colour='black'):
     """Plot a column of data on a given axis."""
-    ax.plot(x_data, y_data, color=color, lw=0.75)
-    ax.set_ylabel(y_label, color=color, fontsize=FONT_SIZE)
-    ax.tick_params(axis='y', color=color, labelcolor=color, size=FONT_SIZE, labelsize=FONT_SIZE)
-    ax.set_xlim(x_data[-1], x_data[0])  # This will ensure descending x-axis
+    ax.plot(x_data, y_data, label=label, color=colour, lw=0.75)
 
-def plot_data(data):
+def plot_residuals(ax, x_data, y_data, colour='black'):
+    """Plot a column of data on a given axis."""
+    ax.fill_between(x_data, 0, y_data, color=colour, alpha=0.5)
+
+
+def plot_data(files, output_file):
     """Generate plots for the provided data."""
-    wavenumbers = data[:, 0]
-    fig, ax1 = plt.subplots(figsize=FIG_SIZE)
-    plot_column(ax1, wavenumbers, data[:, COLUMNS[0][0]], COLUMNS[0][1], COLUMNS[0][2])
+    fig = plt.figure(figsize=FIG_SIZE)
+    gs = gridspec.GridSpec(2, 1, height_ratios=[2, 1]) 
+    ax1 = plt.subplot(gs[0])
+    ax2 = plt.subplot(gs[1], sharex=ax1)
+
+    data1 = read_plt_file(files[0])
+    data2 = read_plt_file(files[1])
     
-    ax2 = ax1.twinx()
-    plot_column(ax2, wavenumbers, data[:, COLUMNS[1][0]], COLUMNS[1][1], COLUMNS[1][2])
-    
-    ax1.set_xlabel(r'Wavenumbers ($cm^{-1}$)', size=FONT_SIZE)
+    if data1 is not None and data2 is not None:
+        wavenumbers = data1[:, 0]
+        radiance1 = data1[:, RADIANCE_COLUMN]
+        radiance2 = data2[:, RADIANCE_COLUMN]
+        difference = ((radiance2 - radiance1)/radiance1) * 100
+        
+        plot_radiance(ax1, wavenumbers, radiance1, LABELS[0], COLOURS[0])
+        plot_radiance(ax1, wavenumbers, radiance2, LABELS[1], COLOURS[1])
+        
+        plot_residuals(ax2, wavenumbers, difference)
+        
+        for iax, ax in enumerate([ax1, ax2]):
+            ax.set_ylabel(YLABELS[iax], fontsize=FONT_SIZE)
+            ax.set_yscale(SCALE)
+            ax.tick_params(axis='y', labelsize=FONT_SIZE)
+            ax.set_xlim(wavenumbers[-1], wavenumbers[0])  # This will ensure descending x-axis
+            ax.grid(ls='--', lw=0.5, color='grey')
+        ax2.set_ylim(-6, 6)
+        # ax2.plot([max(wavenumbers), min(wavenumbers)], [0, 0], ls='--', lw=1, color='black')
+
+
+    ax1.legend(fontsize=FONT_SIZE)
+    ax2.set_xlabel(r'Wavenumbers ($cm^{-1}$)', size=FONT_SIZE)
     plt.tight_layout()
-    plt.savefig(f"{OUTPUT_FILE}.png", dpi=300)
+    plt.savefig(output_file, dpi=300)
 
 def main():
     """Main execution function."""
-    data = read_plt_file(INPUT_FILE)
-    if data is not None:
-        plot_data(data)
+    files = [os.path.join(DATA_DIR, file) for file in INPUT_FILES]
+    plot_data(files, OUTPUT_FILE)
 
 if __name__ == "__main__":
     main()
---
+++ 4aop/plot_4aop_radiance_brightness_temperature.py
@@ -0,0 +1,60 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+# Constants
+DATA_DIR = "C:\\Users\\padra\\Documents\\Research\\projects\\contrails\\4aop\\outputs\\"
+INPUT_FILES = [
+    "spc4asub0001_inv_co2_CAMS_test_iasil1c_icehr20iasi1c.plt",
+    "spc4asub0001_inv_co2_CAMS_test_iasil1c_ice_test_icehr20iasi1c.plt"
+]
+OUTPUT_FILE = f"{DATA_DIR}ice_vs_noice.png"  # Add your output directory here
+
+COLUMNS = [
+    (1, 'Radiance (W $m^{-2} sr^{-1} cm^{-1}$)', 'log', 'black'),
+    (2, 'Brightness Temperature (K)', 'linear', 'red'),
+]
+FIG_SIZE = (12, 6)
+FONT_SIZE = 10
+
+def read_plt_file(filename):
+    """Load data from filename, handling possible errors."""
+    try:
+        return np.loadtxt(filename)
+    except (IOError, ValueError) as e:
+        print(f"Error reading {filename}: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+    return None
+
+def plot_column(ax, x_data, y_data, y_label, yscale, color):
+    """Plot a column of data on a given axis."""
+    ax.plot(x_data, y_data, color=color, lw=0.75)
+    ax.set_ylabel(y_label, color=color, fontsize=FONT_SIZE)
+    ax.set_yscale(yscale)
+    ax.tick_params(axis='y', color=color, labelcolor=color, size=FONT_SIZE, labelsize=FONT_SIZE)
+    ax.set_xlim(x_data[-1], x_data[0])  # This will ensure descending x-axis
+
+def plot_data(files, output_file):
+    """Generate plots for the provided data."""
+    fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, figsize=FIG_SIZE)
+
+    for file in files:
+        data = read_plt_file(file)
+        if data is not None:
+            wavenumbers = data[:, 0]
+            plot_column(ax1, wavenumbers, data[:, COLUMNS[0][0]], COLUMNS[0][1], COLUMNS[0][2], COLUMNS[0][3])
+            plot_column(ax2, wavenumbers, data[:, COLUMNS[1][0]], COLUMNS[1][1], COLUMNS[1][2], COLUMNS[1][3])
+
+    ax1.set_xlabel(r'Wavenumbers ($cm^{-1}$)', size=FONT_SIZE)
+    ax2.set_xlabel(r'Wavenumbers ($cm^{-1}$)', size=FONT_SIZE)
+    plt.tight_layout()
+    plt.savefig(output_file, dpi=300)
+
+def main():
+    """Main execution function."""
+    files = [os.path.join(DATA_DIR, file) for file in INPUT_FILES]
+    plot_data(files, OUTPUT_FILE)
+
+if __name__ == "__main__":
+    main()
