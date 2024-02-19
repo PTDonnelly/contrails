d50b262252a880e120b63fed776dbcfc53912d31
Padraig Donnelly <padraig2.pd@gmail.com>
2024-02-19 17:06:54 +0100

    edit

--- iasi/plot_covariance.py
+++ iasi/plot_covariance.py
@@ -4,23 +4,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 data = pd.read_csv('nedt.csv', sep='\t')
-
-# Convert to DataFrame
 df = pd.DataFrame(data)
+print(df.head(10))
 
-# Plotting
-plt.figure(figsize=(10, 6))
-plt.plot(df['NEDT'].index, df['NEDT'])
-plt.title('IASI Noise Equivalent Differential Temperature', fontsize=15)
-plt.xlabel('Spectral Channel', fontsize=15)
-plt.ylabel('NEDT (K)', fontsize=15)
-plt.xlim([0, 8461])
-plt.ylim([0.01, 11])
-plt.yscale('log')
-plt.tick_params(axis='both', labelsize=15)
-plt.grid(True)
-plt.savefig('nedt.png')
+# # Plotting
+# plt.figure(figsize=(10, 6))
+# plt.plot(df['NEDT'].index, df['NEDT'])
+# plt.title('IASI Noise Equivalent Differential Temperature', fontsize=15)
+# plt.xlabel('Spectral Channel', fontsize=15)
+# plt.ylabel('NEDT (K)', fontsize=15)
+# plt.xlim([0, 8461])
+# plt.ylim([0.01, 11])
+# plt.yscale('log')
+# plt.tick_params(axis='both', labelsize=15)
+# plt.grid(True)
+# plt.savefig('nedt.png')
 
-
-
-data = pd.read_csv('covariance_matrix', sep='\t')
+data = pd.read_csv('covariance_matrix.csv', sep='\t')
+df = pd.DataFrame(data)
+print(df.head(10))
