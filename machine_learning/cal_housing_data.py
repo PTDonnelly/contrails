1502a70e5fe02087f58272a923027e16065677bf
Padraig Donnelly <padraig2.pd@gmail.com>
2024-02-29 17:58:46 +0100

    edit

---
+++ machine_learning/cal_housing_data.py
@@ -0,0 +1,29 @@
+# # housing.frame.hist(figsize=(12, 10), bins=30, edgecolor="black")
+        # # plt.subplots_adjust(hspace=0.7, wspace=0.4)
+        # # plt.show()
+
+
+        # rng = np.random.RandomState(0)
+        # indices = rng.choice(
+        #     np.arange(housing.frame.shape[0]), size=500, replace=False
+        # )
+
+        # # Drop the unwanted columns
+        # columns_drop = ["Longitude", "Latitude"]
+        # subset = housing.frame.iloc[indices].drop(columns=columns_drop)
+        # # Quantize the target and keep the midpoint for each interval
+        # subset["MedHouseVal"] = pd.qcut(subset["MedHouseVal"], 6, retbins=False)
+        # subset["MedHouseVal"] = subset["MedHouseVal"].apply(lambda x: x.mid)
+
+        # # Create a pair plot
+        # sns.pairplot(data=subset,
+        #              hue="MedHouseVal",
+        #              kind='scatter',
+        #              diag_kind='kde',
+        #              palette="viridis"
+        # )
+
+        # plt.suptitle('Pair Plot of California Housing Data', y=1.02)  # Adjust title position
+        # plt.show()
+
+        # exit()
--- machine_learning/tune_network_reduced.py
+++ machine_learning/tune_network_reduced.py
@@ -17,7 +17,7 @@ from keras.regularizers import l1_l2
 import pandas as pd
 from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.utils import shuffle
 import tensorflow as tf
 import keras_tuner as kt
@@ -27,7 +27,7 @@ from keras_tuner.tuners import Hyperband
 
 class HyperModelTuner(HyperModel):
     def __init__(self, config: dict):
-        self.performance_metrics = config["performance_metrics"]
+        self.performance_metrics = self.define_performance_metrics()
         self.validation_split = config["validation_split"]
         self.max_epochs = config["max_epochs"]
         self.number_of_best_trials = config["number_of_best_trials"]
@@ -47,11 +47,12 @@ class HyperModelTuner(HyperModel):
     def get_training_and_test_data(self, state_seed: int=42) -> None:
         # Load the dataset
         housing = fetch_california_housing(as_frame=True)
-        X = housing.data
+
+        X = housing.data#.drop(columns=["Longitude", "Latitude"])
         y = housing.target
 
         # Combine X and y into a single DataFrame for easier filtering
-        data = pd.concat([X, y], axis=1)
+        data = housing.frame
 
         # Filter out rows where the target (y) is 5 or more
         filtered_data = data[data['MedHouseVal'] < 5]
@@ -67,12 +68,27 @@ class HyperModelTuner(HyperModel):
         return
     
     def scale_input_features(self):
-        # Apply normalistion to input features such that they vary in the range [0, 1]
-        scaler = MinMaxScaler()
+        # # Apply normalistion to input features such that they vary in the range [0, 1]
+        # scaler = MinMaxScaler()
+        # self.X_train_scaled = scaler.fit_transform(self.X_train)
+        # self.X_test_scaled = scaler.transform(self.X_test)
+
+        # Initialize the StandardScaler
+        scaler = StandardScaler()
         self.X_train_scaled = scaler.fit_transform(self.X_train)
         self.X_test_scaled = scaler.transform(self.X_test)
         return
 
+    def define_performance_metrics(self):
+        return [
+            {"name": "loss", "direction": "min"},
+            {"name": "mae", "direction": "min"},
+            {"name": "mse", "direction": "min"},
+            {"name": "val_loss", "direction": "min"},
+            {"name": "val_mae", "direction": "min"},
+            {"name": "val_mse", "direction": "min"}
+        ]
+    
     def build(self, hp):
         model = Sequential()
         
@@ -81,15 +97,18 @@ class HyperModelTuner(HyperModel):
         
         # Dynamic addition of layers
         for i in range(hp.Int('n_layers', 1, 3)):
-            model.add(Dense(units=hp.Int(f'units_{i}', min_value=16, max_value=64, step=16),
+            model.add(Dense(units=hp.Int(f'units_{i}', min_value=16, max_value=32, step=8),
                             activation='relu'))
 
-        
+            # model.add(BatchNormalization())
+            # model.add(Dropout(rate=hp.Float(f'dropout_rate_{i}', **self.hyperparameters['dropout_rate'])))
+    
         # Output layer
         model.add(Dense(1, activation='linear'))
         
         # Optimizer selection
-        optimizer = Adam(learning_rate=hp.Choice('lr_adam', [1e-2, 1e-3, 1e-4]))
+        # optimizer = Adam(learning_rate=hp.Choice('lr_adam', [1e-2, 1e-3, 1e-4, 1e-5, 1e-6]))
+        optimizer = Adam(learning_rate=1e-4)
 
         # Extract metric names for model compilation, validation metrics tracked automatically by keras
         metrics_list = [
@@ -220,51 +239,57 @@ def plot_best_results(hyper_model: HyperModelTuner) -> None:
         current_history_df = history_df[history_df['Rank'] == rank]
         current_predictions_df = predictions_df[predictions_df['Rank'] == rank]
 
-        # Extract predicted and true values
-        predictions = current_predictions_df['Predicted value'].values
-        true_values = current_predictions_df['True value'].values
-        # Fit a linear regression model to the predictions vs. true values
-        lin_reg = LinearRegression().fit(predictions.reshape(-1, 1), true_values)
-        r_squared = lin_reg.score(predictions.reshape(-1, 1), true_values)
-        line_x = np.linspace(predictions.min(), predictions.max(), 100)
-        line_y = lin_reg.predict(line_x.reshape(-1, 1))
+        if not current_predictions_df.empty and len(current_predictions_df) > 1:
 
-        # Create plots similar to the above with current_history_df and predictions
-        fig, axs = plt.subplots(2, 2, figsize=(8, 8))
+            # Extract predicted and true values
+            predictions = current_predictions_df['Predicted value'].values
+            true_values = current_predictions_df['True value'].values
+            # Fit a linear regression model to the predictions vs. true values
+            lin_reg = LinearRegression().fit(predictions.reshape(-1, 1), true_values)
+            r_squared = lin_reg.score(predictions.reshape(-1, 1), true_values)
+            line_x = np.linspace(predictions.min(), predictions.max(), 100)
+            line_y = lin_reg.predict(line_x.reshape(-1, 1))
 
-        # Plot training & validation loss values
-        axs[0, 0].plot(current_history_df['Epoch'], current_history_df['loss'], label='Training')
-        axs[0, 0].plot(current_history_df['Epoch'], current_history_df['val_loss'], label='Validation')
-        axs[0, 0].set_title('Model Loss over Test Epochs')
-        axs[0, 0].set_ylabel('Loss')
-        axs[0, 0].set_xlabel('Epoch')
-        axs[0, 0].legend(loc='upper right')
+            # Create plots similar to the above with current_history_df and predictions
+            fig, axs = plt.subplots(2, 2, figsize=(8, 8))
 
-        # Leave the bottom right plot (1,0) empty
-        axs[1, 1].axis('off')
+            # Plot training & validation loss values
+            axs[0, 0].plot(current_history_df['Epoch'], current_history_df['loss'], label='Training')
+            axs[0, 0].plot(current_history_df['Epoch'], current_history_df['val_loss'], label='Validation')
+            axs[0, 0].set_title('Model Loss over Test Epochs')
+            axs[0, 0].set_ylabel('Loss')
+            axs[0, 0].set_yscale('log')
+            axs[0, 0].set_xlabel('Epoch')
+            axs[0, 0].legend(loc='upper right')
 
-        # Plot training & validation MSE
-        axs[1, 0].plot(current_history_df['Epoch'], current_history_df['mse'], label='Train MSE')
-        axs[1, 0].plot(current_history_df['Epoch'], current_history_df['val_mse'], label='Validation MSE')
-        axs[1, 0].set_title('Model MSE over Test Epochs')
-        axs[1, 0].set_ylabel('MSE')
-        axs[1, 0].set_xlabel('Epoch')
-        axs[1, 0].legend(loc='upper right')
+            # Leave the bottom right plot (1,0) empty
+            axs[1, 1].axis('off')
 
-        # Scatter plot of predicted vs true values in the top right plot
-        axs[0, 1].scatter(true_values, predictions, s=2, marker='.', alpha=0.3)
-        axs[0, 1].plot([true_values.min(), true_values.max()], [true_values.min(), true_values.max()], 'k--', lw=2)  # x=y line
-        axs[0, 1].plot(line_x, line_y, color='red', label='Linear Fit')  # Linear fit
-        axs[0, 1].text(0.95, 0.95, f'$R^2 = {r_squared:.2f}$', fontsize=12, va='top', ha='right', transform=axs[0, 1].transAxes)
-        axs[0, 1].set_title('Predicted vs True Values')
-        axs[0, 1].set_xlabel('True Values')
-        axs[0, 1].set_ylabel('Predicted Values')
-        axs[0, 1].axis('square')  # Force square aspect ratio
+            # Plot training & validation MSE
+            axs[1, 0].plot(current_history_df['Epoch'], current_history_df['mse'], label='Train MSE')
+            axs[1, 0].plot(current_history_df['Epoch'], current_history_df['val_mse'], label='Validation MSE')
+            axs[1, 0].set_title('Model MSE over Test Epochs')
+            axs[1, 0].set_ylabel('MSE')
+            axs[1, 0].set_yscale('log')
+            axs[1, 0].set_xlabel('Epoch')
+            axs[1, 0].legend(loc='upper right')
 
-        plt.tight_layout()
-        plt.savefig(os.path.join(hyper_model.output_directory, f'performance_and_predictions_rank_{rank}.png'))
-        plt.close()  # Close the plot to free memory
+            # Scatter plot of predicted vs true values in the top right plot
+            axs[0, 1].scatter(true_values, predictions, s=2, marker='.', alpha=0.3)
+            axs[0, 1].plot([true_values.min(), true_values.max()], [true_values.min(), true_values.max()], 'k--', lw=2)  # x=y line
+            axs[0, 1].plot(line_x, line_y, color='red', label='Linear Fit')  # Linear fit
+            axs[0, 1].text(0.95, 0.95, f'$R^2 = {r_squared:.2f}$', fontsize=12, va='top', ha='right', transform=axs[0, 1].transAxes)
+            axs[0, 1].set_title('Predicted vs True Values')
+            axs[0, 1].set_xlabel('True Values')
+            axs[0, 1].set_ylabel('Predicted Values')
+            axs[0, 1].axis('square')  # Force square aspect ratio
 
+            plt.tight_layout()
+            plt.savefig(os.path.join(hyper_model.output_directory, f'performance_and_predictions_rank_{rank}.png'))
+            plt.close()  # Close the plot to free memory
+        else:
+            print(f"Not enough data for plotting results. Number of samples: {len(predictions_df)}")
+    return
 
 def main():
     # Read global parameters from JSON configuration file
