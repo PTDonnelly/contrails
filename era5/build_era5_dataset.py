d18d7c3c2f863af5a153bdbe2c73af58d893e1b1
Padraig Donnelly <padraig2.pd@gmail.com>
2024-03-28 16:01:25 +0100

    Update build_era5_dataset.py

--- era5/build_era5_dataset.py
+++ era5/build_era5_dataset.py
@@ -34,7 +34,7 @@ def process_era5_files(variables_dict, start_year, end_year, start_month, end_mo
                     # Create daily averages
                     ds_daily = ds_coarse.resample(time='1D').mean()
 
-                    print(ds['cc'].values)
+                    print(ds_daily['cc'].values)
                     exit()        
                     # Write to new NetCDF file
                     ds_daily.to_netcdf(f"{output_file}.nc", compute=False)
