2e36e6352c47195d6006bbf49080518cf59693e9
Padraig Donnelly <padraig2.pd@gmail.com>
2024-04-03 19:50:04 +0200

    Create submit_job.sh

---
+++ era5/submit_job.sh
@@ -0,0 +1,15 @@
+"""#!/bin/bash
+#SBATCH --job-name=ERA5
+#SBATCH --output=/data/pdonnelly/era5/build_era5.log
+#SBATCH --time=12:00:00
+#SBATCH --ntasks=1
+#SBATCH --mem=8GB
+
+# Purge all modules to prevent conflict with current environnement
+module purge
+
+# Load necessary modules
+module load python/meso-3.8
+
+python /data/pdonnelly/github/pisco/pisco/build_era5_dataset.py
+"""
