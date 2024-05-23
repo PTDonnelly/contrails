812f7b15cd7e95419d6261b396f0bed65ff35f5c
Padraig Donnelly <padraig2.pd@gmail.com>
2024-05-23 16:57:23 +0200

    Update construct_timeseries_with_analogues.py

--- iasi/construct_timeseries_with_analogues.py
+++ iasi/construct_timeseries_with_analogues.py
@@ -25,11 +25,7 @@ def downscale_map(data):
     # Use pd.cut to assign bins
     data['lat_bin'] = pd.cut(data['Latitude'], bins=lat_bins, labels=lat_bins[:-1], include_lowest=True, right=False)
     data['lon_bin'] = pd.cut(data['Longitude'], bins=lon_bins, labels=lon_bins[:-1], include_lowest=True, right=False)
-    
-    # Group by the new bins and calculate the mean
-    data_5x5 = data.groupby(['lat_bin', 'lon_bin'], observed=True)['OLR_mean'].mean().reset_index()
-    
-    return data, data_5x5
+    return data
 
 def adjust_longitude(lon):
     return ((lon + 180) % 360 - 180)
@@ -89,13 +85,20 @@ def remove_duplicates_from_data(analogue_df, analogue_file_path, best_date):
     return analogue_df
 
 def plot_maps(iasi_path, original_df, reconstructed_df, target_date):
-    fig, axes = plt.subplots(3, 1, figsize=(6, 9))
+    fig, axes = plt.subplots(2, 1, figsize=(6, 6))
 
     # Pivot original data set
-    original_pivot = original_df.pivot(index='Latitude', columns='Longitude', values='OLR_mean')
+    original_pivot = original_df.pivot(index='Latitude', columns='Longitude', values='OLR_mean')    
+    # # Drop the column where Longitude = 0 and the row where Latitude = 60
+    # original_pivot = original_pivot.drop(columns=0, errors='ignore')
+    # original_pivot = original_pivot.drop(index=60, errors='ignore')
 
     # Pivot reconstructed data set
     reconstructed_pivot = reconstructed_df.pivot(index='Latitude', columns='Longitude', values='OLR_mean')
+    # Reindex the reconstructed pivot to match the original
+    reconstructed_pivot = reconstructed_pivot.reindex(index=original_pivot.index, columns=original_pivot.columns)
+    # Fill NaNs with the column mean
+    reconstructed_pivot = reconstructed_pivot.apply(lambda x: x.fillna(np.nan), axis=0)
     # Convert the Date column to datetime format
     reconstructed_df['Date'] = pd.to_datetime(reconstructed_df['Date'])
     # Convert the dates to numerical format for coloring
@@ -105,9 +108,20 @@ def plot_maps(iasi_path, original_df, reconstructed_df, target_date):
     lon_bins = np.arange(-60, 1, 1)
 
     # Original map
-    im0 = axes[0].pcolormesh(lon_bins, lat_bins, original_pivot.values, cmap='cividis', shading='auto')
-    axes[0].set_title(f'Original Map: {target_date}')
-    fig.colorbar(im0, ax=axes[0], shrink=0.75)
+    # im0 = axes[0].pcolormesh(lon_bins, lat_bins, original_pivot.values, cmap='cividis', shading='auto')
+    # axes[0].set_title(f'Original Map: {target_date}')
+    # fig.colorbar(im0, ax=axes[0], shrink=0.75)
+
+    # # Add 5-degree gridlines
+    # axes[0].set_xticks(np.arange(-60, 1, 5))
+    # axes[0].set_yticks(np.arange(30, 61, 5))
+    # axes[0].grid(which='both', color='gray', linestyle='--', linewidth=0.5)
+    # axes[0].set_aspect('equal', 'box')
+
+    # Reconstructed map
+    im1 = axes[0].pcolormesh(lon_bins, lat_bins, reconstructed_pivot.values, cmap='cividis', shading='auto')
+    axes[0].set_title(f'Reconstructed IIR: Analogues from 2013-2019')
+    fig.colorbar(im1, ax=axes[0], shrink=0.75)
 
     # Add 5-degree gridlines
     axes[0].set_xticks(np.arange(-60, 1, 5))
@@ -115,35 +129,24 @@ def plot_maps(iasi_path, original_df, reconstructed_df, target_date):
     axes[0].grid(which='both', color='gray', linestyle='--', linewidth=0.5)
     axes[0].set_aspect('equal', 'box')
 
-    # Reconstructed map
-    im1 = axes[1].pcolormesh(lon_bins, lat_bins, reconstructed_pivot.values, cmap='cividis', shading='auto')
-    axes[1].set_title(f'Reconstructed IIR: Analogues from 2013-2019')
-    fig.colorbar(im1, ax=axes[1], shrink=0.75)
-
-    # Add 5-degree gridlines
+    # Scatter plot of reconstructed data
+    scatter = axes[1].scatter(reconstructed_df['Longitude'], reconstructed_df['Latitude'], 
+                              c=date_nums, cmap='jet', marker='o', s=10)
+    axes[1].set_title('Reconstructed IIR: Analogues from 2013-2019')
+    fig.colorbar(scatter, ax=axes[1], label='Date', format=mdates.DateFormatter('%Y'))
     axes[1].set_xticks(np.arange(-60, 1, 5))
     axes[1].set_yticks(np.arange(30, 61, 5))
     axes[1].grid(which='both', color='gray', linestyle='--', linewidth=0.5)
     axes[1].set_aspect('equal', 'box')
-
-    # Scatter plot of reconstructed data
-    scatter = axes[2].scatter(reconstructed_df['Longitude'], reconstructed_df['Latitude'], 
-                              c=date_nums, cmap='jet', marker='o', s=10)
-    axes[2].set_title('Reconstructed IIR: Analogues from 2013-2019')
-    fig.colorbar(scatter, ax=axes[2], label='Date', format=mdates.DateFormatter('%Y'))
-    axes[2].set_xticks(np.arange(-60, 1, 5))
-    axes[2].set_yticks(np.arange(30, 61, 5))
-    axes[2].grid(which='both', color='gray', linestyle='--', linewidth=0.5)
-    axes[2].set_aspect('equal', 'box')
     # Set the limits for the scatter plot
-    axes[2].set_xlim(lon_bins.min(), lon_bins.max())
-    axes[2].set_ylim(lat_bins.min(), lat_bins.max())
+    axes[1].set_xlim(lon_bins.min(), lon_bins.max())
+    axes[1].set_ylim(lat_bins.min(), lat_bins.max())
 
     plt.tight_layout()
     plt.savefig(os.path.join(iasi_path, f"reconstructed_IIR_{target_date}.png"), bbox_inches='tight', dpi=300)
     plt.close()
 
-def construct_daily_map(target_date, iasi_path, combined_df):
+def construct_daily_mean(target_date, iasi_path, combined_df):
     # Read the OLR data for the target date
     year, month, day = target_date.split('-')
     iasi_file_path = os.path.join(iasi_path, year, month.zfill(2), day.zfill(2), 'spectra_and_cloud_products_binned.csv')
@@ -152,7 +155,7 @@ def construct_daily_map(target_date, iasi_path, combined_df):
         iasi_df = pd.read_csv(iasi_file_path, sep="\t", engine='python')
     else:
         return None
-    iasi_df, iasi_scaled_df = downscale_map(iasi_df)
+    iasi_df = downscale_map(iasi_df)
 
     reconstructed_iasi_df = pd.DataFrame()
 
@@ -167,35 +170,30 @@ def construct_daily_map(target_date, iasi_path, combined_df):
                 analogue_file_path = os.path.join(iasi_path, analogue_year, analogue_month.zfill(2), analogue_day.zfill(2), 'spectra_and_cloud_products_binned.csv')
 
                 if os.path.exists(analogue_file_path):
-                    # print(i, "File exists: use analogue")
                     analogue_df = pd.read_csv(analogue_file_path, sep="\t", engine='python')
                     analogue_df = remove_duplicates_from_data(analogue_df, analogue_file_path, best_date)
-                    analogue_df, analogue_scaled_df = downscale_map(analogue_df)
+                    analogue_df = downscale_map(analogue_df)
 
                     # Extract 1x1 degree data for the 5x5 degree grid cell
                     matched_df = analogue_df[(analogue_df['lat_bin'] == lat_bin) &
                                             (analogue_df['lon_bin'] == lon_bin)]
                     
                     if not matched_df.empty:
-                        print(lat_bin, lon_bin, best_date)
-                        # print(matched_df.head())
-                        # # Print all unique values in the 'lat_bin' column
-                        # unique_lon = analogue_df['Longitude'].unique()
-                        # print(f"Unique lon values for {best_date}: {unique_lon}")
-                        # unique_lon_bins = analogue_df['lon_bin'].unique()
-                        # print(f"Unique lon_bin values for {best_date}: {unique_lon_bins}")
-                        # input()
                         reconstructed_iasi_df = pd.concat([reconstructed_iasi_df, matched_df])
                         break  # Exit the loop once a valid file is found
-                
+    
+    # Plot for posterity
     plot_maps(iasi_path, iasi_df, reconstructed_iasi_df, target_date)
-    exit()
-    return reconstructed_iasi_df
+    
+    # Calculate the average OLR for the reconstructed data
+    reconstructed_daily_average = reconstructed_iasi_df['OLR_mean'].mean()
+    return reconstructed_daily_average
 
 def main():
     # Base paths for data
     iasi_path = 'G:\\My Drive\\Research\\Postdoc_2_CNRS_LATMOS\\data\\iasi\\binned_olr'
     analogues_path = "G:\\My Drive\\Research\\Postdoc_2_CNRS_LATMOS\\data\\circulation_analogues"
+    output_path = "G:\\My Drive\\Research\\Postdoc_2_CNRS_LATMOS\\data\\machine_learning"
     
     # Date range for Spring 2020
     spring_2020_dates = pd.date_range(start='2020-03-16', end='2020-04-13')
@@ -204,7 +202,7 @@ def main():
     days = spring_2020_dates.day
 
     # Construct the new time series
-    new_iasi_timeseries = pd.DataFrame()
+    daily_average_list = []
 
     for year, month, day in zip(years, months, days):
         target_date = f"{year}-{month:02d}-{day:02d}"
@@ -215,14 +213,28 @@ def main():
         # Check if both files exist
         if os.path.exists(dates_file_path) and os.path.exists(cor_file_path):
             combined_df = extract_analogues_and_correlations(dates_file_path, cor_file_path)
-            daily_map = construct_daily_map(target_date, iasi_path, combined_df)
-    #         if daily_map is not None:
-    #             # Calculate the average OLR for the reconstructed data
-    #             new_daily_average = daily_map['OLR_mean'].mean()
-    #             new_iasi_timeseries = pd.concat([new_iasi_timeseries, new_daily_average])
+            daily_average = construct_daily_mean(target_date, iasi_path, combined_df)
+            daily_average_list.append({'Date': target_date, 'OLR_mean': daily_average})
     
-    # # Save the new time series to a CSV file
-    # new_iasi_timeseries.to_csv('new_iasi_timeseries.csv', index=False)
+    # Convert daily_average_list to a DataFrame and save
+    daily_average_df = pd.DataFrame(daily_average_list)
+
+    # Read in the era5_iasi_combined file
+    era5_iasi_combined_df = pd.read_csv(os.path.join(output_path, "era5_iasi_combined.csv"), sep='\t')
+
+    # Merge the new daily averages into the existing DataFrame based on the Date column
+    era5_iasi_combined_df['Date'] = pd.to_datetime(era5_iasi_combined_df['Date'])
+    daily_average_df['Date'] = pd.to_datetime(daily_average_df['Date'])
+    
+    # Identify the dates that need updating
+    dates_to_update = daily_average_df['Date']
+
+    # Update only the rows that need changing
+    mask = era5_iasi_combined_df['Date'].isin(dates_to_update)
+    era5_iasi_combined_df.loc[mask, 'OLR_mean'] = era5_iasi_combined_df.loc[mask, 'Date'].map(daily_average_df.set_index('Date')['OLR_mean'])
+
+    # Save the updated DataFrame to a new file
+    era5_iasi_combined_df.to_csv(os.path.join(output_path, "era5_iasi_combined_corrected.csv"), sep='\t', index=False)
 
 if __name__ == "__main__":
     main()
